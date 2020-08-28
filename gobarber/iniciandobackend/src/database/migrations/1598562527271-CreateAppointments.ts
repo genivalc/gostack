febe6b8a3efd52667d225ca3c46cef0dbb1d2142
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-08-27 21:51:46 -0300

    inicio do gobarbe usando db

--- gobarber/iniciandobackend/src/database/migrations/1598562527271-CreateAppointments.ts
+++ gobarber/iniciandobackend/src/database/migrations/1598562527271-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1598562527271 implements MigrationInterfa
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
