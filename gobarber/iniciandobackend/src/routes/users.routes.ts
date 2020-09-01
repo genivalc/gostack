99be1179a36c9d1986157400a0b681903c5a072d
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-01 17:33:57 -0300

    criando classe de erro

--- gobarber/iniciandobackend/src/routes/users.routes.ts
+++ gobarber/iniciandobackend/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -31,7 +32,20 @@ usersRoutes.post('/', async (request, response) => {
 
 usersRoutes.patch(
   '/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-  return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.fieldname,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
 });
 
 export default usersRoutes;
