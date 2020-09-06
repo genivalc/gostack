fabc4a5c8a2eefecdab3c4837ec7ab591a5a720f
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-05 23:45:22 -0300

    pagina de login

--- gobarber/gobarber-web/src/App.tsx
+++ gobarber/gobarber-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SinIn from './pages/Signin';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-  <h1>Hello world</h1>
+  <SinIn />
   <GlobalStyle />
   </>
 );
