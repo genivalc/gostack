64f3076acd7a3b68753e8aa5e676d56884c2171a
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-08 17:05:42 -0300

    web login pelo contexto

--- gobarber/gobarber-web/src/pages/Signin/index.tsx
+++ gobarber/gobarber-web/src/pages/Signin/index.tsx
@@ -1,9 +1,10 @@
-import React, {useCallback, useRef} from 'react';
+import React, {useCallback, useRef, useContext} from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,10 +14,17 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async(data: object) => {
+  const { signIn } = useContext(AuthContext);
+
+  const handleSubmit = useCallback(async(data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -30,12 +38,15 @@ const SignIn: React.FC = () => {
         abortEarly: false,
       });
 
-
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
     } catch (err) {
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }
-  }, []);
+  }, [signIn]);
 
 
   return (
