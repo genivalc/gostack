aaa85b045b131cdfdc079e9e51b18b160004927c
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-22 16:06:26 -0300

    Refatorando o back end do gorbaber

--- gobarber/iniciandobackend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber/iniciandobackend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
+
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async(request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointment = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async(request, response) => {
+//   const appointment = await appointmentsRepository.find();
 
-  return response.json(appointment);
-});
+//   return response.json(appointment);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
 
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
+  const appointmentsRepository = new AppointmentsRepository();
 
-    const createAppointment = new CreateAppointmentService();
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
