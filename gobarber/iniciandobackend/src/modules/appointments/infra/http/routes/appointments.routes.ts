0362fa14545654f5300dc9219c6bb7ae7404b43f
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-23 14:37:52 -0300

    Configurando o jest no back end

--- gobarber/iniciandobackend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber/iniciandobackend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,37 +1,15 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async(request, response) => {
-//   const appointment = await appointmentsRepository.find();
-
-//   return response.json(appointment);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-
-    const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-  //vai caregar o service, verificar se precisa de alguma dependecia
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
-
-    return response.json(appointment);
-
-});
+appointmentsRouter.post('/', appointmentsController.create) ;
 
 export default appointmentsRouter;
