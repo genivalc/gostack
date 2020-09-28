013eaeb396aa8a5a96960566f90d44b44e60ba0d
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-28 19:48:10 -0300

    Atualização de Avatar teste

--- gobarber/iniciandobackend/src/modules/users/services/UpdateUserAvatarService.ts
+++ gobarber/iniciandobackend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import fs from 'fs';
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/provider/StorageProvide/models/IStorageProvider';
 
 import uploadConfig from '@config/upload';
 import User from '../infra/typeorm/entities/User';
@@ -19,7 +20,12 @@ interface IRequest{
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository) { }
+    private usersRepository: IUsersRepository,
+
+  @inject('StorageProvider')
+  private storageProvider: IStorageProvider,
+
+  ){}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
@@ -29,15 +35,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.save(user);
 
